371c53516a6c0ff5f153d5e325b6cdb1acb1bf9b
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-13 07:19:43 +0500

    Updating the table constraints

--- schema.sql
+++ schema.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
-id INT PRIMARY KEY, name VARCHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL
+id INT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL
  );
