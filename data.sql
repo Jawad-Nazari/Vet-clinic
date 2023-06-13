44055b843bdb7c87444b44d2db824dbdc1a80183
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-13 09:56:25 +0500

    Remving the whitespace

--- data.sql
+++ data.sql
@@ -4,5 +4,5 @@
  VALUES (1,'Agumon', date '2020-02-03', 0, true ,10.23),
         (2,'Gabumon', date '2018-11-15', 2, true, 8.00), 
         (3,'Pikachu', date '2021-01-07', 1, false, 15.04), 
-        (4,'Devimon ', date '2017-05-12', 5, true, 11.00);
+        (4,'Devimon', date '2017-05-12', 5, true, 11.00);
      
