914cb9b76987b93f93c05887a5dabc892b7ca71a
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-14 11:43:43 +0500

    Updated animals table by adding species column

--- schema.sql
+++ schema.sql
@@ -3,5 +3,13 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals(
-id INT PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL
+id INT PRIMARY KEY NOT NULL, 
+name VARCHAR(50) NOT NULL, 
+date_of_birth DATE, 
+escape_attempts INT, 
+neutered BOOLEAN, 
+weight_kg DECIMAL
  );
+
+/*  Updated animals table by adding species column   */
+   ALTER TABLE animals ADD COLUMN species VARCHAR(50);  
