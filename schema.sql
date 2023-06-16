6833a26b0796127b71c11ea3495fb3b7206d6e5f
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-16 11:32:25 +0500

    Adding new tables

--- schema.sql
+++ schema.sql
@@ -43,3 +43,42 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+
+ /* Create a table named vets with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string
+age: integer
+date_of_graduation: date  */
+
+
+CREATE TABLE vets (
+     id SERIAL PRIMARY KEY, 
+     name VARCHAR(50),
+     age INT, 
+     date_of_graduation DATE
+    );
+
+ /* There is a many-to-many relationship between the tables species and vets: 
+a vet can specialize in multiple species, 
+and a species can have multiple vets specialized in it. 
+Create a "join table" called specializations to handle this relationship. */
+
+CREATE TABLE specializations (
+     vet_id INT, species_id INT, 
+     PRIMARY KEY (vet_id , species_id) ,
+     FOREIGN KEY (vet_id) REFERENCES vets (id), 
+     FOREIGN KEY (species_id) REFERENCES species (id)
+   );
+
+ /* There is a many-to-many relationship between the tables animals and vets: 
+an animal can visit multiple vets and one vet can be visited by multiple animals. 
+Create a "join table" called visits to handle this relationship, 
+it should also keep track of the date of the visit. */
+
+CREATE TABLE visits (
+    animal_id INT, vet_id INT, visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id),
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+   );
